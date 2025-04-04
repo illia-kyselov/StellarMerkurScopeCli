3aead926958161741f9bbe83a10c8ed208f15d27
User1 <user1@cmadmins-MacBook-Pro.local>
2025-04-04 07:34:26 +0300

    added redux-pertsist

--- App.jsx
+++ App.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './src/navigation/TabNavigator';
-import { store } from './src/store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './src/store/store';
 
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={{ flex: 1 }}>
-                <NavigationContainer>
-                    <TabNavigator />
-                </NavigationContainer>
-            </View>
+            <PersistGate loading={null} persistor={persistor}>
+                <View style={{ flex: 1 }}>
+                    <NavigationContainer>
+                        <TabNavigator />
+                    </NavigationContainer>
+                </View>
+            </PersistGate>
         </Provider>
     );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import levelsReducer from './slices/levelsSlice';
 import eventsReducer from './slices/eventsSlice';
 import observationsReducer from './slices/observationsSlice';
 import unitReducer from './slices/unitSlice';
 
-export const store = configureStore({
-    reducer: {
-        levels: levelsReducer,
-        events: eventsReducer,
-        observations: observationsReducer,
-        unit: unitReducer,
-    }
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+    levels: levelsReducer,
+    events: eventsReducer,
+    observations: observationsReducer,
+    unit: unitReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
